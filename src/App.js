bcb474a8fdc746ae8388af2bd2c73d93aedd2eee
Arshia Mathur <mathur.arshia99@gmail.com>
2020-06-30 22:44:01 -0400

    Added components and style folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
+import Homepage from './components/Homepage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Welcome to FitHawk
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     Welcome to FitHawk
+    //   </header>
+    // </div>
+    <Homepage></Homepage>
   );
 }
 
---
+++ src/components/Homepage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Grid from 'semantic-ui-react'
+
+class Homepage extends React.Component {
+
+
+    render(){
+        return(
+            <div>
+                <header>            
+            Homepage
+            </header>
+            {/* <Grid>
+                <Grid.Row>
+                    <Grid.Column width = {3}>
+
+                    </Grid.Column>
+
+                </Grid.Row>
+
+            </Grid> */}
+
+
+
+            </div>
+            
+
+        )
+    }
+
+        
+    
+
+
+}
+
+export default Homepage;
+
