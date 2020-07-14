adf7702dd7bd5e8eef73527dcc55d87785d629f0
Arshia Mathur <mathur.arshia99@gmail.com>
2020-07-14 19:11:17 -0400

    made app.js a component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import './App.css';
 import Homepage from './components/Homepage'
 
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     Welcome to FitHawk
-    //   </header>
-    // </div>
-    <Homepage></Homepage>
-  );
-}
+class App extends React.Component {
+  render(){
+    return (
+      // <div className="App">
+      //   <header className="App-header">
+      //     Welcome to FitHawk
+      //   </header>
+      // </div>
+
+      <Homepage></Homepage>
+    );
+
+  }
+    
+  
+} 
 
 export default App;
