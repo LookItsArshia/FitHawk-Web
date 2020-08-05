71ebc3b85d355298eb0a9099cc01e8784cf44951
kamran14 <kam@kamrant.com>
2020-08-04 21:39:43 -0400

    Merged master

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -7,8 +7,6 @@ import ReactPlayer from 'react-player'
 
 class InfoPage extends React.Component {
 
-
-
     render() {
     return (
         
@@ -32,31 +30,23 @@ class InfoPage extends React.Component {
                                 <ReactPlayer url="http://104.154.143.76/video/video=ZIGuuXU2+t=01:10" playing="true"></ReactPlayer>
             </Grid.Column>
             </Grid.Row>
-
         </Grid>
         </div>
 
 
 
 
-        <div className="bottomText">
+        <div className="bottomText" >
             <Grid.Row>
-                    <p>Paragraph explaining app:
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, 
-                        purus sit amet luctus venenatis, lectus magna fringilla Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, 
-                        purus sit amet luctus venenatis, lectus magna fringilla Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, </p>
+                    <p>FitHawk was inspired by its creators desire to work out smarter, not harder! 
+                        Attending Wilfrid Laurier University in Waterloo, Ontario, Canada, like-minded computer science enthusiasts 
+                        Ryan Ottman, Ray Zhao, Matthew Dietrich, Colin Faught, Kamran Tayyab and Arshia Mathur connected to bring their vision to life! 
+                        Bringing low cost, high efficiency workout analysis to the everyday gym goer, it is our hope that FitHawk will inspire others to break through their barriers, 
+                        and give them the feedback to accompany their drive as they strive to take their workouts to the next level.</p>
             </Grid.Row>
         </div>
-
-        <div className="bigButton">
-            <Button style={{padding: '15px 32px'}}>Back to Profile</Button>
-        </div>
-
-
-
-        </Grid>
-        </div>
-
+            </Grid>
+            </div>
         )
     }
 }
